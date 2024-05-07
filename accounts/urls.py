a424e5eb8da4d722b3ff24eb304a66c130cb7b2b
Park SungHyun <sunghyun0620@gmail.com>
2024-05-07 09:48:33 +0900

    accounts:write_articleGET 작성글, 작성댓글, 좋아요 글, 좋아요 댓글 생각한 로직 및 URL 설정 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path("<str:username>/", views.UserDetailAPIView.as_view(), name="profile"),
+    # path("<str:username>/articles/", views.WriteArticleAPIView.as_view(), name="write_article"),
+    # path("<str:username>/comments/", views.WriteCommentAPIView.as_view(), name="write_comment"),
+    # path("<str:username>/articles-favorite/", views.FavoriteArticleAPIView.as_view(), name="favorite_article"),
+    # path("<str:username>/comments-favorite/", views.FavoriteCommentAPIView.as_view(), name="favorite_comment"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,34 @@ from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
+# from articles.models import Article, Comment
 
 class UserDetailAPIView(APIView):
     def get(self, request, username):
         user = get_object_or_404(get_user_model(), username=username)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+# class WriteArticleAPIView(APIView):
+#     def get(self, request,username):
+#         articles=get_object_or_404(Article,author=username)
+#         serializer = UserSerializer(articles)
+#         return Response(serializer.data)
+
+# class WriteCommentAPIView(APIView):
+#     def get(self, request,username):
+#         comments=get_object_or_404(Comment,author=username)
+#         serializer = UserSerializer(comments)
+#         return Response(serializer.data)
+
+# class FavoriteArticleAPIView(APIView):
+#     def get(self, request,username):
+#         articles=get_object_or_404(Article,favorite=username)
+#         serializer = UserSerializer(articles)
+#         return Response(serializer.data)
+
+# class FavoriteCommentAPIView(APIView):
+#     def get(self, request,username):
+#         comments=get_object_or_404(Comment,favorite=username)
+#         serializer = UserSerializer(comments)
+#         return Response(serializer.data)
