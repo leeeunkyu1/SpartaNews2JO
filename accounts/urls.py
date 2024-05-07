31ccc373260166546e4065576ba566611a2fc780
DarkestTiger <june140133@gmail.com>
2024-05-07 19:59:13 +0900

    accounts:유저,회원가입 완성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from accounts import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('api/signup/', views.UserSignup.as_view(), name='signup'),
+    path('api/login/', views.UserLogin.as_view(), name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserSignup(APIView):
+    def post(self, request):
+        print(request.data)
+        serializer = UserSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogin(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 200:
+            user = response.data.get('user')
+            refresh = RefreshToken.for_user(user)
+            response.data['refresh'] = str(refresh)
+            response.data['access'] = str(refresh.access_token)
+        return response
