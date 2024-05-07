74b3266bcf11c1d87844c80c5b3ae7057fa0c1d0
이은규 <djkyteofficial@gmail.com>
2024-05-07 12:54:10 +0900

    작성자 필드 수정중

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,17 +1,19 @@
+from urllib import request
 from rest_framework import serializers
 from .models import Article
+from django.contrib.auth import get_user_model
 
-
-
+user = get_user_model()
 
 class ArticleSerializer(serializers.ModelSerializer):
+    user = request.user(read_only=True)
 
     class Meta:
         model = Article
         fields = "__all__"
-        
-        
-        
+
+
+
         
         
         
