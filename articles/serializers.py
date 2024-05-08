5f9add50c4d178e804508e9d5526c56769fdd172
이은규 <djkyteofficial@gmail.com>
2024-05-08 11:08:30 +0900

    댓글 조회 머지 완료

--- articles/serializers.py
+++ articles/serializers.py
@@ -7,12 +7,16 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
+        
+        
 class ArticleDetailSerializer(ArticleSerializer):
     pass
 
 class CommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     article = ArticleSerializer(read_only=True)
+    
+    
     class Meta:
         model = Comment
         fields = "__all__"
