5ae582ba61f074e3ab714106bb759b537b85643d
Park SungHyun <sunghyun0620@gmail.com>
2024-05-08 15:00:26 +0900

    accounts:commentlistGET 작성 댓글 리스트 조회 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("<str:username>/", views.UserDetailAPIView.as_view(), name="profile"),
     path("<str:username>/articles/", views.WriteArticleAPIView.as_view(), name="write_article"),
-    # path("<str:username>/comments/", views.WriteCommentAPIView.as_view(), name="write_comment"),
+    path("<str:username>/comments/", views.WriteCommentAPIView.as_view(), name="write_comment"),
     path("<str:username>/articles-favorite/", views.FavoriteArticleAPIView.as_view(), name="favorite_article"),
     # path("<str:username>/comments-favorite/", views.FavoriteCommentAPIView.as_view(), name="favorite_comment"),
 ]
