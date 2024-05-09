9b12b02554eab47b1b59c7287ffe3cc67e1697eb
DarkestTiger <june140133@gmail.com>
2024-05-09 15:28:41 +0900

    accounts:User 로그아웃

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from accounts import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView
 )
 
 
@@ -11,11 +12,12 @@ app_name = 'accounts'
 urlpatterns = [
     path('api/signup/', views.UserSignup.as_view(), name='signup'),
     path('api/login/', views.UserLogin.as_view(), name='login'),
+    path('api/logout/', TokenBlacklistView.as_view(), name='logout'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("<str:username>/", views.UserDetailAPIView.as_view(), name="profile"),
     path("<str:username>/articles/", views.WriteArticleAPIView.as_view(), name="write_article"),
-    path("<str:username>/comments/", views.WriteCommentAPIView.as_view(), name="write_comment"),
+    # path("<str:username>/comments/", views.WriteCommentAPIView.as_view(), name="write_comment"),
     path("<str:username>/articles-favorite/", views.FavoriteArticleAPIView.as_view(), name="favorite_article"),
-    path("<str:username>/comments-favorite/", views.FavoriteCommentAPIView.as_view(), name="favorite_comment"),
+    # path("<str:username>/comments-favorite/", views.FavoriteCommentAPIView.as_view(), name="favorite_comment"),
 ]
