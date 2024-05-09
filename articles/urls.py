9ae7e17097c76843fc3b8bf7befd0a4c61a67d56
ryuh2929 <ryuh2929@gmail.com>
2024-05-09 14:51:16 +0900

    articles:comment 좋아요

--- articles/urls.py
+++ articles/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<int:article_pk>/", views.ArticleDetailAPIView.as_view(), name="article_detail"),
     path('<int:article_pk>/comments/', views.CommentView.as_view(), name='comment'),
     path('<int:article_pk>/comments/<int:comment_pk>/', views.CommentDetailView.as_view(), name='comment_detail'),
+    path('<int:article_pk>/comments/<int:comment_pk>/favorite/', views.CommentFavoriteView.as_view(), name='comment_favorite'),
 ]
